aa8438dad18a4ea05860eaf50caa719e3aa068f3
Беликова Нелли <nelli.128@mail.ru>
2023-02-27 23:52:12 +0300

    лаба 1))))

---
+++ main.py
@@ -0,0 +1,66 @@
+'''password = input("Введите пароль: ")
+
+if len(password) > 12:
+    print("Слишком длинный пароль")
+
+elif password(0:13) == "ninello"
+    print("Пароль надежный")
+else:
+    print("OK")'''
+
+
+
+
+
+'''seat = int(input('Введите номер места в плацкартном вагоне: '))
+print()
+
+if range (36,54):
+    print("Одиночное место")
+
+elif seat % 2:
+    print("Ваше место верхнее")
+
+else:
+    print("Ваше место нижнее")'''
+
+
+
+
+
+'''year = int(input())
+print()
+
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+print("Год весокосный")
+else:
+print("Год не весокосный")'''
+
+
+
+
+'''colors = ('красный', 'синий', 'желтый')
+c1 = input()
+c2 = input()
+if c1 in colors and c2 in colors:
+    if c1 != c2:
+
+    if (c1 in ("красный", "синий")) and (c2 in ("красный", "синий")):
+        print("фиолетовый")
+    if (c1 in ("желтый", "красный")) and (c2 in ("желтый", "красный")):
+        print("оранжевый")
+    if (c1 in ("синий", "желтый")) and (c2 in ("синий", "желтый")):
+        print("зеленый")
+    else:
+        print(c1)
+    else:
+    print("ошибка цвета")'''
+
+
+
+'''a = int(input('Введите нужное количество слов: '))
+result = ''
+for i in range (a):
+    x = input(f"ввод слов {i + 1}:")
+    result += x + " "
+print("результат:", result.strip())'''
